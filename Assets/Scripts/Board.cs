e1b725e5f9c708605ab9d81f997950609f51ba64
na_cho <ryoya.19970625@gmail.com>
2019-09-02 18:03:40 +0900

    途中経過

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] public GameObject[] PieceData;
+    private Vector3[] birthPos;
+    private int MaxPieces = 60;
+    private float interbal = 10.0f;
+    private GameObject[] Pieces;
+    private bool[] isPieces;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pieces = new GameObject[MaxPieces];
+        isPieces = new bool[MaxPieces];
+        birthPos = new Vector3[6];
+        for(int i = 0; i < 6; i++)
+        {
+            birthPos[i] = new Vector3(-2.6f + (1.3f * i), 7.8f, 0.0f);
+        }
+        
+        for (int i = 0; i < MaxPieces; i++)
+        {
+            isPieces[i] = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interbal -= Time.deltaTime;
+
+        // パズルの移動と生成をする
+        if (interbal <= 0.0f)
+        {
+            int birth = 0;
+            interbal = 10.0f;
+            for (int i = 0; i < MaxPieces; i++)
+            {
+                if (isPieces[i])
+                {
+                    Vector3 pos = Pieces[i].transform.position;
+                    Pieces[i].transform.position = new Vector3(pos.x, pos.y - 1.3f, pos.z);
+                }
+                else if(birth != 6)
+                {
+                    int number = Random.Range(0, 6);
+                    Pieces[i] = Instantiate(PieceData[number], birthPos[birth], Quaternion.identity);
+                    isPieces[i] = true;
+                    birth++;
+                }
+
+            }
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    private int kind;
+    private float speed;
+    private bool isShot;
+
+    SpriteRenderer MainSpriteRenderer;
+    public Sprite One;
+    public Sprite Two;
+    public Sprite Three;
+    public Sprite Four;
+    public Sprite Five;
+    public Sprite Six;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        kind = 0;
+        isShot = false;
+        ChangeBullet(kind);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 弾発射
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isShot = true;
+        }
+
+        // 当たった時元に戻る
+        if(transform.position.y >= 5.0f)
+        {
+            isShot = false;
+            transform.position = new Vector3(transform.position.x, -3.0f, transform.position.z);
+        }
+
+        if (isShot)
+        {
+            MoveBullet(0.0f, 0.5f);
+            return;
+        }
+
+        // 左に移動
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveBullet(-1.3f, 0.0f);
+        }
+
+        // 右に移動
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveBullet(1.3f, 0.0f);
+        }
+
+        // タイプチェンジ
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            kind++;
+            if(kind % 6 == 0)
+            {
+                kind = 0;
+            }
+            ChangeBullet(kind);
+
+        }
+        
+    }
+
+    //
+    // 弾のチェンジ
+    //
+    private void ChangeBullet(int kind)
+    {
+       switch(kind)
+        {
+            case 0:
+                MainSpriteRenderer.sprite = One;
+                break;
+            case 1:
+                MainSpriteRenderer.sprite = Two;
+                break;
+            case 2:
+                MainSpriteRenderer.sprite = Three;
+                break;
+            case 3:
+                MainSpriteRenderer.sprite = Four;
+                break;
+            case 4:
+                MainSpriteRenderer.sprite = Five;
+                break;
+            case 5:
+                MainSpriteRenderer.sprite = Six;
+                break;
+            default:
+                break;
+        }
+    }
+
+    //
+    // 移動
+    //
+    private void MoveBullet(float x, float y)
+    {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x + x, pos.y + y, pos.z);
+    }
+
+}
